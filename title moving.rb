b4d58c68d0a6b890eca4a6b39bcc49d1bb7ea0e3
Hyperlight <hyperlux@users.noreply.github.com>
2025-04-29 18:27:44 +0200

    new shapes

---
+++ title moving.rb
@@ -0,0 +1,7 @@
+title moving
+transparent header more
+bigger? "Créer des expériences numériques audacieuses, créatives et fluides."
+remove container for "Français"
+make content appears as we scroll down, and pop up , maybe snap?
+carrerarallye.com preview in portfolio
+remove form? app script
