ff11076c1a66bc95e1608789280c955f1a7c1c9a
Hyperlight <hyperlux@users.noreply.github.com>
2025-05-01 11:39:41 +0200

    better portfolio card text

--- title moving.rb
+++ title moving.rb
@@ -1,7 +1,3 @@
-title moving?
-transparent header more
-bigger? "Créer des expériences numériques audacieuses, créatives et fluides."
-remove container for "Français"
-make content appears as we scroll down, and pop up , maybe snap?
+
 carrerarallye.com preview in portfolio
-remove form? app script
+remove form? or app script to send email to me
